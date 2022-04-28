db95cd3a51a98eba784a576f2e682db6e7984385
DEV - OTAVIO FREIRE <MAC1005@macbarbosa.local>
2022-04-28 17:35:33 -0300

    feat: Aprendendo sobre JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from "./Button"
 import "./styles.css"
 
-const App = () =>{
+function soma(a, b){
+    alert(a + b)
 
+}
+
+function App() {
     return (
         <div className="App">
             Hello World
+            <Button onClick={() => soma(10,20)} name="Otavio Freire"/>
         </div>
     )
 }
